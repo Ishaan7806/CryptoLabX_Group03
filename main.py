31e16ad604d930df727ac0dc65ac45789bcb1221
2024ucp1260 <2024ucp1260@mnit.ac.in>
2026-08-07 08:06:45 +0530

    Update main.py for task 6

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+
+def write_log(option):
+    with open("logs.txt", "a") as f:
+        now = datetime.now()
+        f.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - {option}\n")
+
 def menu():
     while True:
         print("\n===== CryptoLabX =====")
@@ -10,16 +17,27 @@ def menu():
         choice = input("Enter your choice: ")
 
         if choice == "1":
+            write_log("Encrypt")
             print("Encrypt - Coming Soon")
+
         elif choice == "2":
+            write_log("Decrypt")
             print("Decrypt - Coming Soon")
+
         elif choice == "3":
+            write_log("Attack")
             print("Attack - Coming Soon")
+
         elif choice == "4":
+            write_log("Analyze")
             print("Analyze - Coming Soon")
+            # Call your Task 4 file analysis function here if you want
+
         elif choice == "5":
+            write_log("Exit")
             print("Goodbye!")
             break
+
         else:
             print("Invalid choice")
 
